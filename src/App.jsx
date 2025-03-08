ea3e8cd64fa2cd086207de823f76b78499c94bb4
Emre Çalışkan <114865537+emrecaliskan1@users.noreply.github.com>
2025-03-08 13:15:53 +0300

    implement localStorage for todos management and add useEffect for syncing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +8,14 @@ import TodoList from './components/TodoList'
 
 function App() {
   
-  const [todos,setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const createTodo = (newTodo) => {
     setTodos([...todos,newTodo]);
